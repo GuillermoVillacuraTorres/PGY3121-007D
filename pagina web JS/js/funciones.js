18930d428412e3d2439549b08496960a62928025
Guillermo Villacura <gu.villacura@gmail.com>
2023-04-05 20:46:08 -0400

    Clase 05-04-2023 JS

---
+++ pagina web JS/js/funciones.js
@@ -0,0 +1,49 @@
+console.log("Hola mundo");
+//alert("Bienvenidos");
+
+
+let var1 = 10;
+let var2 = 15;
+let var3 = 20;
+
+if (var1 > 5 && var2 < 50 && var3 >=10 ) {
+    
+}
+if (var1 > 5 || var2 < 50 || var3 >=10 ) {
+    
+}
+
+
+if (var1 === var2) {
+    console.log("Son iguales.");
+}else{
+    console.log("No son iguales.");
+}
+
+
+if (var1 == 10) {
+    
+}else if (var2 ==1){
+
+}
+
+const constante = 100;
+
+console.log(constante);
+
+
+
+function miFuncion(){
+    console.log("Mi primera funcion");
+}
+
+miFuncion();
+
+
+function funcionParametros(num1,num2){
+    resultado = num1 + num2;
+    console.log("El resultado es: ", resultado);
+}
+
+funcionParametros(10,50);
+
