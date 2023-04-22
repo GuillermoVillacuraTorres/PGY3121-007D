781e07df1105df78ab43964d0a1ba44ca7fcaa67
Guillermo Villacura <g.villacurat@gmail.com>
2023-04-21 22:21:38 -0400

    Clase 21-04-2023

--- pagina web JS/js/array.js
+++ pagina web JS/js/array.js
@@ -45,5 +45,55 @@ miArrayTres.unshift({id:5,nombre:"Los Lagos"});
 console.log(miArrayTres);
 
 for (const j of miArrayTres) {
-    //console.log(j);
+    //console.log(j.nombre);
+}
+
+console.table(miArrayTres);
+
+let filtro = miArrayTres.filter(e => e.id == 1 || e.id == 5);
+console.log("filtro", filtro);
+
+function cargaRegion(){
+    const select = document.getElementById("cmbRegion");
+    for (const i of miArrayTres) {
+        const option = document.createElement("option");
+        option.value = i.id;
+        option.textContent = i.nombre;
+        select.appendChild(option);
+
+    }
+}
+
+
+function load(){
+    cargaRegion();
+    api();
+    apiParametro();
+}
+
+function valorSelect(){
+    let valor = document.getElementById("cmbRegion").value;
+    console.log("Valor select: ", valor);
+}
+
+
+function api(){
+    fetch('https://mindicador.cl/api')
+    .then(response => response.json())
+    .then(data => {
+        //console.log(data);
+        let dolar = document.getElementById("dolar");
+
+        dolar.innerHTML += data.dolar.valor;
+
+    })
+}
+
+
+function apiParametro(){
+    fetch('https://mindicador.cl/api/' + 'dolar')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
 }
