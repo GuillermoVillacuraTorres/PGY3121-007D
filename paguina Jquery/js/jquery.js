befc2bf0fe3648107626c922f0c3d3da22b0542f
Guillermo Villacura <g.villacurat@gmail.com>
2023-05-10 20:24:43 -0400

    Clase 10-05-2023

--- paguina Jquery/js/jquery.js
+++ paguina Jquery/js/jquery.js
@@ -3,5 +3,75 @@ $(document).ready(function(){
 })
 
 $(function(){
-    console.log("Ready");
+    $("p").css({"background-color" : "blue"});
+    $(".primero").css({"background-color" : "red"});
+    $("#tercero").css({"background-color" : "green"});
+})
+
+
+$("#ocultar").on("click",function(){
+    //$(".primero").hide();
+    //$(".primero").fadeOut();
+    $(".primero").slideUp();
+})
+
+
+$("#mostrar ").on("click",function(){
+    //$(".primero").show();
+    //$(".primero").fadeIn();
+    $(".primero").slideDown();
+})
+
+
+$("#texto").on("click",function(){
+    //$(".primero").text("Bienvenidos!");
+    //$(".primero").html("<strong>Bienvenidos!!!</strong>");
+    //$(".primero").append("<strong>Bienvenidos!!!</strong>")
+    $(".primero").prepend("<strong>Inicio!!!</strong>")
+})
+
+
+
+$("#texto").on("click",function(){
+    let valor = $("#txtValor").val();
+    console.log(valor);
+});
+
+
+$(function(){
+    $("#miFormulario").validate({
+        rules:{
+            txtRut:{
+                required: true,
+                minlength:10
+            },
+            txtNombre:{
+                required: true,
+                minlength:10
+            },
+            txtApellido:{
+                required: true,
+                minlength:10
+            },
+            txtCorreo:{
+                required: true,
+                minlength:10,
+                email:true
+            }
+        },
+        messages:{
+            txtRut:{
+                required: "Debe ingresar un rut",
+                minlength: "El largo minimo es 10"
+            },
+        }
+    })
+})
+
+
+
+$.getJSON('https://mindicador.cl/api', function(data){
+    console.log(data);
+}).fail(function(){
+    console.error("Error al consumir la API");
 })
