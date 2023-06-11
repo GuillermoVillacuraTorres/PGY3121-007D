112569b4394864314b35d4310b4927b1333cdc72
Guillermo Villacura <g.villacurat@gmail.com>
2023-06-10 21:28:46 -0400

    Clase 09-06-2023

--- django/apps/Tienda/views.py
+++ django/apps/Tienda/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 
 def cargarInicio(request):
-    return render(request,"inicio.html")
+    productos = Producto.objects.all()
+    categoria_perros = Producto.objects.filter(id_categoria = 1)
+    categoria_gatos = Producto.objects.filter(id_categoria = 2)
+    return render(request,"inicio.html",{"prod":productos,"cate_gatos":categoria_gatos,"cate_perros":categoria_perros})
+    
+
+
+def cargarAgregarProducto(request):
+    categorias = Categoria.objects.all()
+    return render(request,"agregarProducto.html",{"cate":categorias})
